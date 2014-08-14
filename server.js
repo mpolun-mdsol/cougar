b23491c1a790f81dc476af634d37deba9d5b0d6f
Max Polun <mpolun@mdsol.com>
2014-08-14 14:46:20 -0400

    added static file serving

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+'use strict'
 var express = require('express'),
-    app = express(),
+    serveStatic = require('serve-static')
+
+var app = express(),
     port = process.env.PORT || 7777,
     router = express.Router()
 
-router.get('/', function(req, res) {
+// serve front-end
+app.use(serveStatic('app/'))
+
+router.get('/', function serveDefault(req, res) {
   res.json({content: 'Hello World!'})
 })
 
 app.use('/', router)
 app.listen(port)
 console.log('cougar loading on port', port)
-
