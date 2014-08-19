3fff14c80c3d124ae774c4bb3c624b5886848fc4
Andy Yong <ayong@mdsol.com>
2014-08-19 16:16:24 -0400

    saving before wiping folder

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,20 @@ require('angular')
 require('angular-strap')
 require('angular-strap.tpl')
 
-console.log('hello world!')
+var cougar = angular.module('cougar', [])
+
+cougar.controller('PlayersCtrl', function ($scope, $http) {
+  var app = this;
+
+  function addPlayer(player) {
+    if (player) {
+      $http.post('http://localhost:7777/api/players/', player, {headers: {'Content-Type': 'application/json'}})
+      .success(function playerCreated(data) {
+        console.log(data)
+      })
+      .error(function playerCreateError(data) {
+        console.log(data)
+      })
+    }
+  }
+})
