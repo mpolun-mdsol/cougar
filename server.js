a9bdee88bae60f6516ed981df0b90ef179f875db
Andy Yong <ayong@mdsol.com>
2014-08-20 15:45:05 -0400

    adds crud methods for teams api. adds add, delete and get teams on the client side

--- server.js
+++ server.js
@@ -16,12 +16,14 @@ app.use(bodyParser.json())
 app.set('bookshelf', bookshelf)
 
 // Models
-var Player = require('models/players')
+var Player = require('models/players'),
+    Team = require('models/teams')
 
 router.get('/', function serveDefault(req, res) {
   res.json({content: 'Hello World this is a test!'})
 })
 
+// Players
 router.get('/api/players', function servePlayers(req, res) {
   Player.fetchAll().then(function playersFetched(players) {
     res.json(players)
@@ -54,6 +56,42 @@ router.route('/api/players/:player_id')
   var player = req.player.id
   req.player.destroy().then(function playerDestroyed() {
     res.send('Player ' + player + ' has been destroyed')
+  })
+})
+
+// Teams
+router.get('/api/teams', function serveTeams(req, res) {
+  Team.fetchAll().then(function teamsFetched(teams) {
+    res.json(teams)
+  })
+})
+
+router.post('/api/teams', function createTeam(req, res) {
+  Team.forge(req.body).save().then(function teamCreated(team) {
+    res.json(team)
+  })
+})
+
+router.param('team_id', function fetchTeam(req, res, next, id) {
+  Team.where('id', id).fetch().then(function teamFetched(team) {
+    req.team = team
+    next()
+  })
+})
+
+router.route('/api/teams/:team_id')
+.get(function serveTeam(req, res) {
+  res.json(req.team)
+})
+.put(function updateTeam(req, res) {
+  req.team.set(req.body).save().then(function teamUpdated(team) {
+    res.json(team)
+  })
+})
+.delete(function destroyTeam(req, res) {
+  var team = req.team.id
+  req.team.destroy().then(function teamDestroyed() {
+    res.send('Team ' + team + ' has been destroyed')
   })
 })
 
