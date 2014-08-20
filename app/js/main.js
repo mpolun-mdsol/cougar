36f7d10fd7824ca8b99eeb2bca886136466c5eef
Andy Yong <ayong@mdsol.com>
2014-08-20 10:17:47 -0400

    adds the addplayer function

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
 'use strict'
 require('angular')
 require('angular-strap')
-require('angular-strap.tpl')
+require('angular-strap/dist/angular-strap.tpl')
 
 var cougar = angular.module('cougar', [])
 
 cougar.controller('PlayersCtrl', function ($scope, $http) {
-  $scope.addPlayer(player) {
+  $scope.addPlayer = function (player) {
     if (player) {
       $http.post('http://localhost:7777/api/players/', player, {headers: {'Content-Type': 'application/json'}})
       .success(function playerCreated(data) {
